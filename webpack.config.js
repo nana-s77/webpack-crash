de1a70fab017e64b426683119d27744f449b466d
nana suzuki <nana.suzuki@nijibox.co.jp>
2020-07-27 17:49:44 +0900

    implement getStart

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path'); //pathモジュールを取り込む
+
+//resolve 絶対パスに変換する
+//現在のディレクトリ(user/~)の名前とdistを連結してる
+const outputPath =  path.resolve(__dirname,'dist'); 
+console.log(outputPath);
+
+module.exports = {
+  entry: './src/index.js', //バンドル対象の設定
+  output: {
+    filename: 'main.js',
+    path: outputPath,
+  }
+}
